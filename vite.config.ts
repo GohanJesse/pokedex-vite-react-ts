c278e8c113400d180a017925b2d363cabe751d92
Jessy <jessegodais@gmail.com>
2023-11-10 15:12:10 +0100

    essai avec config vite chemin dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/pokedex-vite-react-ts/",
+  base: "/pokedex-vite-react-ts/dist",
   plugins: [react()],
 });
